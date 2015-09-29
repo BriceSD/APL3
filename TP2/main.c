1b8db39fa455ae7af59d82cfb0d9127c671fb86b
BriceSD <brice.saunier@gmail.com>
2015-09-29 15:09:06 +0200

    Ajout des fonctions vider_buffer() et lire()

--- TP2/main.c
+++ TP2/main.c
@@ -123,6 +123,40 @@ void jouer_partie(int plateau[], int nb_allum){
     }
     afficher_etat(plateau);
   }
+  
+  void viderBuffer()
+{
+    int c = 0;
+    while (c != '\n' && c != EOF)
+    {
+        c = getchar();
+    }
+}
+ 
+int lire(char *chaine, int longueur)
+{
+    char *positionEntree = NULL;
+ 
+    if (fgets(chaine, longueur, stdin) != NULL)
+    {
+        positionEntree = strchr(chaine, '\n');
+        if (positionEntree != NULL)
+        {
+            *positionEntree = '\0';
+        }
+        else
+        {
+            viderBuffer();
+        }
+        return 1;
+    }
+    else
+    {
+        viderBuffer();
+        return 0;
+    }
+}
+
 }
 
 int main(){
